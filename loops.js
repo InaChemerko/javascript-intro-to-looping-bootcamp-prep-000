594c43d6468f2e5117dc1d0d452fb1e2ef19f215
Ina Chemerko <innachemerko@gmail.com>
2017-12-22 03:31:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
 function forLoop (array){
   for (var i = 1; i<26; i++){
-    
+    if (i == 1){
+    array.push("I am 1 strange loops");
+    } else {
+      array.push("I am ${i} strange loops");
+    }
   }
+  return array;
 }
